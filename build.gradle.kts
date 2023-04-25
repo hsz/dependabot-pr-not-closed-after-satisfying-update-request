bc84ec9970ddf2702d3227f1242b1500c53812f2
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2023-04-25 14:23:39 +0200

    switch to properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
-plugins {
-    alias(libs.plugins.kotlin)
-}
+val junitVersionProvider: Provider<String> = providers.gradleProperty("junitVersion")
 
 version = "1.0.0"
 group = "bar"
 
+plugins {
+    `java-library`
+}
+
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("junit:junit:${junitVersionProvider.get()}")
 }
